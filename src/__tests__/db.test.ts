75c254f24087ddf8b0a0336b24686174be5b3432
Gaspard Martin <gaspard.martin.jouannaud@gmail.com>
2025-04-04 15:51:39 +0200

    longer timeout

--- src/__tests__/db.test.ts
+++ src/__tests__/db.test.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import '../config/db';
 
+jest.setTimeout(50000);
+
 describe('Database Connection', () => {
     it('should connect to the database', async () => {
         await mongoose.connection.asPromise(); // Wait for the connection to complete
